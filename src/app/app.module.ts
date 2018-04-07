02c97050ec0e96aeac7a4fcf8c43abc4422a9ec6
Prasanth Venkatesan <onlyyprasanth@gmail.com>
2018-04-07 15:19:05 +0530

    Routing with lazy loading implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
+export const ROUTES: Routes = [
+    { path: '', loadChildren: 'app/pages/pages.module#PagesModule' },
+    { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES, { useHash: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
