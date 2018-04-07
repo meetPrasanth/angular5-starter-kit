1b79dd1154414800df0d9aad371f7b32fadbe944
Prasanth Venkatesan <onlyyprasanth@gmail.com>
2018-04-07 15:20:07 +0530

    Revert "Routing with lazy loading implemented"
    This reverts commit 02c97050ec0e96aeac7a4fcf8c43abc4422a9ec6.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 
-export const ROUTES: Routes = [
-    { path: '', loadChildren: 'app/pages/pages.module#PagesModule' },
-    { path: '**', redirectTo: '' }
-];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    RouterModule.forRoot(ROUTES, { useHash: true })
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,33 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Routes, RouterModule } from '@angular/router';
-
-import { PagesComponent } from './pages.component';
-
-const ROUTES: Routes = [
-    { 
-        path: '', 
-        component: PagesComponent,
-        children : [
-            {
-                path: 'profile',
-                loadChildren: './profile/profile.module#ProfileModule'
-            },{
-                path: '',
-                loadChildren: './home/home.module#HomeModule'
-            }
-        ]
-    },{ 
-        path: '**', 
-        redirectTo: '' 
-    }
-]
 
 @NgModule({
   imports: [
-    CommonModule,
-    RouterModule.forChild(ROUTES)
+    CommonModule
   ],
-  declarations: [PagesComponent]
+  declarations: []
 })
 export class PagesModule { }
